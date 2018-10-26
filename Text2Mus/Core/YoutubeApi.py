0e4d7086c00cfb2affba5cd47f98acab936f2fa6
Jaygem <youness.kafia@devinci.fr>
2018-10-26 23:38:37 +0200

    Added youtube API to get video results (might be useless)
    Added Auto encoder in Playground/GANPG
    The loss doesn't seem to go below 1 needs more testing

--- Text2Mus/Core/YoutubeApi.py
+++ Text2Mus/Core/YoutubeApi.py
@@ -2,25 +2,35 @@ import json
 import sys
 import google.oauth2.credentials
 
+import YoutubeSearch
+
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
 
+DEVELOPER_KEY = "REPLACE_ME"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
 
 commenturl = 'https://www.googleapis.com/youtube/v3/commentThreads'
 searchurl = 'https://www.googleapis.com/youtube/v3/search'
 
 class YoutubeVideo():
-    def __init__(self, id, jsondata):
+    def __init__(self, id):
+
         return
 
 
+class SearchQuery():
+    def __init__(self, query, max_res):
+        self.q = query
+        self.max_results = max_res
+    def search(self):
+        YoutubeSearch.youtube_search(self)
 
 
-class YoutubeApi():
-    def __init__(self):
-        return
-    def YoutubeSearch(self, name):
-        return
-    def getData(self,video):
-        return
+dodo = SearchQuery("bonobo",15)
+dodo.search()
